7c9cd483e4f526bc4ae809e53f598420563fe316
anhbtn <buithingocanh@luvina.net>
2021-02-04 15:25:19 +0700

    commit bt 5 phan 1

---
+++ FinalProjects/Phan 4/Excercise4/src/main/java/todolist/entity/Enroll.java
@@ -0,0 +1,28 @@
+package todolist.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "enrolls")
+public class Enroll {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer enrollId;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+}
